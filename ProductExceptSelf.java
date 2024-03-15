cbc3cea0ebd1bcef4a0d7ceac28f58f43d99a73c
Sujit Jayaraj <35429693+sujitjayaraj@users.noreply.github.com>
2024-03-15 13:00:13 +0530

    solved daily problem 238

---
+++ ProductExceptSelf.java
@@ -0,0 +1,19 @@
+//Question 238 named Product of Array Except Self
+
+class ProductExceptSelf
+{
+    public int[] productExceptSelf(int[] nums){
+        int product = 1;
+        int[] result = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            result[i] = product;
+            product = product * nums[i];
+        }
+        product = 1;
+        for(int i=nums.length-1; i>=0; i--){
+            result[i] *= product;
+            product = product * nums[i];
+        }
+        return result;
+    }
+}
